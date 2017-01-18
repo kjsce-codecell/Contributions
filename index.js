11c3047bc07573a4fa02bac6753b9b072ae5cb17
Chaitya Shah <Chaitya62@gmail.com>
2017-01-19 01:35:14 +0530

    Final 1.0

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var getData = (function($){
 	$.get(URL,function(data,status){
 			console.log(data);
 			var img;
-			var thumbnail_b = "<a class='thumbnail' href='";
+			var thumbnail_b = "<a class='thumbnail' target='_blank' href='";
 			var thumbnail_m = "'>";
 			var thumbnail_a = "</a>";
 			var img_b = "<img src='";
 			var img_a = "' alt='' class='img-responsive'>";
-			var container_b = "<div class='col-md-3 col-xs-3' id='author'><div class='container-fluid'>";
+			var container_b = "<div class='col-md-3 col-xs-6 col-lg-3' id='author'><div class='container-fluid'>";
 			var container_a = "</div>";	
 			var caption_b =  "<div class='caption'>";
 			var caption_a = "</div></div>";
@@ -21,17 +21,22 @@ var getData = (function($){
 				author = d.author;
 				//console.log(d.commit.message);
 				var mes = d.commit.message;
+				var name = d.commit.author.name;
+				if(name == ""){
+					name = mes;
+				}
 				var date = d.commit.committer.date;
-				if(mes=="Initial commit") {
+
+				if(mes=="Initial commit" && author.login == "vedipen") {
 					return true;
-				}	
+				}		
 				console.log(date);
 				// img = "<img src=" + author.avatar_url + "/>  <h2>" +author.login +"</h2> <h3>"+mes+"</h3>"
 				var template = 
 				container_b +
 					thumbnail_b + d.author.html_url + thumbnail_m + 
 						img_b + 
-							d.author.avatar_url + 
+							author.avatar_url + 
 						img_a +  
 					thumbnail_a +
 					caption_b +
@@ -39,7 +44,7 @@ var getData = (function($){
 							author.login +
 						h2_a +
 						commit_message_b +
-							mes+
+							name+
 						commit_message_a+
 					caption_a +
 				 container_a;
